66bb45e5f152e06e0f4f577561e9c2839fee0304
DustinRepo <39870946+DustinRepo@users.noreply.github.com>
2023-01-19 21:24:34 -0800

    Removed unused testing code

--- SteamAccountManager/main.cpp
+++ SteamAccountManager/main.cpp
@@ -1,12 +1,9 @@
 #include <Windows.h>
-#include <thread>//sleep
-#include <conio.h>//_getch()
+#include <thread>
 
 #include "util/SteamAccount.h"
 #include "util/SteamUtil.h"
 #include "Window.h"
-
-#define WAITFORUSER printf_s("Press a key..."); _getch()
 
 //Decided to remake an old project I had that no longer worked after the recent
 //Steam login window change. I also decided to try 'snake case' rather than 'hungarian notation'
@@ -16,7 +13,6 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE previous, PSTR args, INT show
 	c_steam_account_manager.load_accounts();
 
 	if (!Window::init_window(instance)) {
-		printf_s("ERORR: Could not init window!");
 		return 0;
 	}
 
